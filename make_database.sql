295de6711cce6ce23fd3f88796636ac607be40a7
Evan Yang <evan.yang@uky.edu>
2019-11-12 00:57:10 -0500

    corrected mistake in sql script

--- make_database.sql
+++ make_database.sql
@@ -9,6 +9,6 @@ USE ecommerce;
 CREATE TABLE Users (userID INT PRIMARY KEY NOT NULL, username VARCHAR(30), password VARCHAR(30), isStaff BOOL, isManager BOOL); 
 CREATE TABLE Products (productID INT PRIMARY KEY NOT NULL, price FLOAT, inventory INT, category VARCHAR(30));
 CREATE TABLE Orders (orderID INT PRIMARY KEY NOT NULL, 
-    FOREIGN KEY (cID) REFERENCES Users(userID), FOREIGN KEY (pID) REFERENCES Products(productID),
+    FOREIGN KEY (userID) REFERENCES Users(userID), FOREIGN KEY (productID) REFERENCES Products(productID),
     status VARCHAR(30), money_saved FLOAT, isCancelled BOOL);
-CREATE TABLE ShoppingBasket (FOREIGN KEY (cID) REFERENCES Users(userID), FOREIGN KEY (pID) REFERENCES Products(productID));
+CREATE TABLE ShoppingBasket (FOREIGN KEY (userID) REFERENCES Users(userID), FOREIGN KEY (productID) REFERENCES Products(productID));
