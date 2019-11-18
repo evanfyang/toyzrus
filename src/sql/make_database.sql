77e92ceacedc6f50d0c2362edf626fa5a0fe6787
Skye Brown <skye.brown@uky.edu>
2019-11-18 11:54:35 -0500

    AUTO INCREMENT keys in db, add Product sample data

--- src/sql/make_database.sql
+++ src/sql/make_database.sql
@@ -6,15 +6,55 @@
 CREATE DATABASE ecommerce;
 USE ecommerce;
 
-CREATE TABLE Users (userID INT PRIMARY KEY NOT NULL, username VARCHAR(30), password VARCHAR(30), isStaff BOOL, isManager BOOL); 
-CREATE TABLE Products (productID INT PRIMARY KEY NOT NULL, price FLOAT, inventory INT, category VARCHAR(30));
-CREATE TABLE Orders (orderID INT PRIMARY KEY NOT NULL, cID INT, pID INT,
-    FOREIGN KEY (cID) REFERENCES Users(userID), FOREIGN KEY (pID) REFERENCES Products(productID),
-    status VARCHAR(30), money_saved FLOAT, isCancelled BOOL);
-CREATE TABLE ShoppingBasket (cID INT, pID INT, 
-    FOREIGN KEY (cID) REFERENCES Users(userID), FOREIGN KEY (pID) REFERENCES Products(productID));
+/* database tables */
+CREATE TABLE Users (userID INT PRIMARY KEY NOT NULL AUTO_INCREMENT, username VARCHAR(40) NOT NULL, password VARCHAR(40) NOT NULL, address VARCHAR(80) NOT NULL, isStaff BOOLEAN NOT NULL, isManager BOOLEAN NOT NULL); 
+CREATE TABLE Products (productID INT PRIMARY KEY NOT NULL AUTO_INCREMENT, name VARCHAR(40) NOT NULL, price FLOAT NOT NULL, inventory INT NOT NULL, category VARCHAR(30) NOT NULL);
+CREATE TABLE Orders (orderID INT PRIMARY KEY NOT NULL AUTO_INCREMENT, userID INT NOT NULL, prodID INT NOT NULL,
+    FOREIGN KEY (userID) REFERENCES Users(userID), FOREIGN KEY (prodID) REFERENCES Products(productID),
+    status VARCHAR(30) NOT NULL, money_saved FLOAT NOT NULL, isCancelled BOOLEAN NOT NULL);
+CREATE TABLE ShoppingBasket (userID INT NOT NULL, prodID INT NOT NULL, 
+    FOREIGN KEY (userID) REFERENCES Users(userID), FOREIGN KEY (prodID) REFERENCES Products(productID));
 
 /* Create initial users */
-INSERT INTO Users (userID, username, password, isStaff, isManager) VALUES (0, 'staff',     'password', TRUE, FALSE);
-INSERT INTO Users (userID, username, password, isStaff, isManager) VALUES (1, 'manager    ', 'password', FALSE, TRUE);
-INSERT INTO Users (userID, username, password, isStaff, isManager) VALUES (2, 'user',     'password', FALSE, FALSE);                                
+INSERT INTO Users (username, password, isStaff, isManager) VALUES ('staff', 'password', TRUE, FALSE);
+INSERT INTO Users (username, password, isStaff, isManager) VALUES ('manager', 'password', TRUE, TRUE);
+INSERT INTO Users (username, password, isStaff, isManager) VALUES ('user', 'password', FALSE, FALSE);   
+
+/*
+Products sample data:
+    - categories: action figures, bikes, dolls, games, outdoors
+ */
+INSERT INTO Products (name, price, inventory, category) VALUES ('Yoda', 34.99, 25, 'action figures');
+INSERT INTO Products (name, price, inventory, category) VALUES ('Spiderman', 8.75, 'action figures');
+INSERT INTO Products (name, price, inventory, category) VALUES ('Superman', 4.47, 'action figures');
+INSERT INTO Products (name, price, inventory, category) VALUES ('Batman', 4.47, 'action figures');
+INSERT INTO Products (name, price, inventory, category) VALUES ('Transformer: Optimus Prime', 30.49, 'action figures');
+INSERT INTO Products (name, price, inventory, category) VALUES ('GI Joe', 15.99, 'action figures');
+
+INSERT INTO Products (name, price, inventory, category) VALUES ('LOL Suprise - Pink Girls', 89.00, 'bikes');
+INSERT INTO Products (name, price, inventory, category) VALUES ('Transformers: Bumblebee - Yellow/Gray Boys', 124.99, 'bikes');
+INSERT INTO Products (name, price, inventory, category) VALUES ('Hot Wheels - Red/Silver Unisex', 98.99, 'bikes');
+INSERT INTO Products (name, price, inventory, category) VALUES ('Disney Princess - Purple Girls', 94.99, 'bikes');
+INSERT INTO Products (name, price, inventory, category) VALUES ('Disney Frozen - Blue Girls', 89.00, 'bikes');
+INSERT INTO Products (name, price, inventory, category) VALUES ('Trolls - Green/Pink Unisex', 74.99, 'bikes');
+
+INSERT INTO Products (name, price, inventory, category) VALUES ('Dora', , 'dolls');
+INSERT INTO Products (name, price, inventory, category) VALUES ('Barbie', , 'dolls');
+INSERT INTO Products (name, price, inventory, category) VALUES ('Bratz', , 'dolls');
+INSERT INTO Products (name, price, inventory, category) VALUES ('Ken', , 'dolls');
+INSERT INTO Products (name, price, inventory, category) VALUES ('American Girl', , 'dolls');
+INSERT INTO Products (name, price, inventory, category) VALUES ('Cinderella', , 'dolls');
+
+INSERT INTO Products (name, price, inventory, category) VALUES ('Pictionary', 16.99, 'games');
+INSERT INTO Products (name, price, inventory, category) VALUES ('Monopoly', 12.59, 'games');
+INSERT INTO Products (name, price, inventory, category) VALUES ('Candyland', 5.99, 'games');
+INSERT INTO Products (name, price, inventory, category) VALUES ('Game of Life', 17.99, 'games');
+INSERT INTO Products (name, price, inventory, category) VALUES ('Hungry Hungry Hippo', 9.89, 'games');
+INSERT INTO Products (name, price, inventory, category) VALUES ('Trouble', 6.99, 'games');
+
+INSERT INTO Products (name, price, inventory, category) VALUES ('Nerf Elite', 9.79, 'outdoors');
+INSERT INTO Products (name, price, inventory, category) VALUES ('Pedal Car', 79.99, 'outdoors');
+INSERT INTO Products (name, price, inventory, category) VALUES ('Walkie Talkies', 29.99, 'outdoors');
+INSERT INTO Products (name, price, inventory, category) VALUES ('Mini Trampoline', 63.00, 'outdoors');
+INSERT INTO Products (name, price, inventory, category) VALUES ('Scooter', 39.99, 'outdoors');
+INSERT INTO Products (name, price, inventory, category) VALUES ('Outdoor Playset', 1399.99, 'outdoors');
