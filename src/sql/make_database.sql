94ef612505c78e6f27d208754741af79a8f5daca
Evan Yang <evan.yang@uky.edu>
2019-12-10 16:32:59 -0500

    updated database for demo

--- src/sql/make_database.sql
+++ src/sql/make_database.sql
@@ -7,20 +7,28 @@ CREATE DATABASE ecommerce;
 USE ecommerce;
 
 /* database tables */
-CREATE TABLE Users (userID INT PRIMARY KEY NOT NULL AUTO_INCREMENT, username VARCHAR(40) NOT NULL, password VARCHAR(40) NOT NULL, firstname VARCHAR(40) NOT NULL, lastname VARCHAR(40) NOT NULL, address VARCHAR(120), isStaff BOOLEAN NOT NULL, isManager BOOLEAN NOT NULL); 
-CREATE TABLE Products (productID INT PRIMARY KEY NOT NULL AUTO_INCREMENT, name VARCHAR(40) NOT NULL, price DECIMAL(10,2) NOT NULL, inventory INT NOT NULL, category VARCHAR(30) NOT NULL, promotions INT NOT NULL);
-CREATE TABLE Orders (orderID INT NOT NULL, userID INT NOT NULL, prodID INT NOT NULL, quantity INT NOT NULL,
-    FOREIGN KEY (userID) REFERENCES Users(userID), FOREIGN KEY (prodID) REFERENCES Products(productID),
-    status VARCHAR(30) NOT NULL, money_saved FLOAT NOT NULL, order_datetime DATETIME NOT NULL);
-CREATE TABLE ShoppingBasket (userID INT NOT NULL, prodID INT NOT NULL, quantity INT NOT NULL, 
-	FOREIGN KEY (userID) REFERENCES Users(userID), FOREIGN KEY (prodID) REFERENCES Products(productID));
+CREATE TABLE Users (userID INT PRIMARY KEY NOT NULL AUTO_INCREMENT, 
+    username VARCHAR(40) NOT NULL, password VARCHAR(40) NOT NULL, 
+    firstname VARCHAR(40) NOT NULL, lastname VARCHAR(40) NOT NULL, 
+    address VARCHAR(120), isStaff BOOLEAN NOT NULL, isManager BOOLEAN NOT NULL); 
+CREATE TABLE Products (productID INT PRIMARY KEY NOT NULL AUTO_INCREMENT, 
+    name VARCHAR(40) NOT NULL, price DECIMAL(10,2) NOT NULL, inventory INT NOT NULL, 
+    category VARCHAR(30) NOT NULL, promotions INT NOT NULL);
+CREATE TABLE Orders (orderID INT NOT NULL, userID INT NOT NULL, prodID INT NOT NULL, 
+    quantity INT NOT NULL, FOREIGN KEY (userID) REFERENCES Users(userID), 
+    FOREIGN KEY (prodID) REFERENCES Products(productID), status VARCHAR(30) NOT NULL, 
+    money_saved FLOAT NOT NULL, order_datetime DATETIME NOT NULL);
+CREATE TABLE ShoppingBasket (userID INT NOT NULL, prodID INT NOT NULL, 
+    quantity INT NOT NULL, FOREIGN KEY (userID) REFERENCES Users(userID), 
+    FOREIGN KEY (prodID) REFERENCES Products(productID));
 
 /* Create initial users */
 INSERT INTO Users (username, password, firstname, lastname, address, isStaff, isManager) VALUES ('staff', 'password', 'Staff', 'Personnel', NULL, TRUE, FALSE);
-INSERT INTO Users (username, password, firstname, lastname, address, isStaff, isManager) VALUES ('manager', 'password', 'Store', 'Manager', NULL, TRUE, TRUE);
+INSERT INTO Users (username, password, firstname, lastname, address, isStaff, isManager) VALUES ('manager', 'password', 'Store', 'Manager', NULL, FALSE, TRUE);
 INSERT INTO Users (username, password, firstname, lastname, address, isStaff, isManager) VALUES ('user', 'password', 'Test', 'User', '329 Rose St., Lexington, KY USA 40508' ,FALSE, FALSE); 
 /* professor access */
-INSERT INTO Users (username, password, firstname, lastname, address, isStaff, isManager) VALUES ('cs405', 'cs405', 'Dr.', 'Liu', '329 Rose St., Lexington, KY USA 40508', TRUE, TRUE); 
+INSERT INTO Users (username, password, firstname, lastname, address, isStaff, isManager) VALUES ('cs405', 'cs405', 'Dr.', 'Liu', '329 Rose St., Lexington, KY USA 40508', FALSE, FALSE); 
+INSERT INTO Users (username, password, firstname, lastname, address, isStaff, isManager) VALUES ('cs405m', 'cs405m', 'Dr.', 'Liu', '329 Rose St., Lexington, KY USA 40508', FALSE, TRUE); 
 
 /*
 Products sample data:
@@ -62,6 +70,10 @@ INSERT INTO Products (name, price, inventory, category, promotions) VALUES ('Sco
 INSERT INTO Products (name, price, inventory, category, promotions) VALUES ('Outdoor Playset', 1399.99, 2, 'outdoors', 0);
 
 /* Sample orders to test customer order page */
+INSERT INTO Orders (orderID, userID, prodID, quantity, status, money_saved, order_datetime) VALUES (1421773477, 3, 22, 1, 'Pending', 0, '2019-11-30 13:00:00');
+INSERT INTO Orders (orderID, userID, prodID, quantity, status, money_saved, order_datetime) VALUES (1421773477, 3, 23, 20, 'Pending', 0, '2019-11-30 13:00:00');
+INSERT INTO Orders (orderID, userID, prodID, quantity, status, money_saved, order_datetime) VALUES (1421773477, 3, 24, 40, 'Pending', 0, '2019-11-30 13:00:00');
+
 INSERT INTO Orders (orderID, userID, prodID, quantity, status, money_saved, order_datetime) VALUES (1421773477, 3, 1, 1, 'Pending', 0, '2019-10-31 13:00:00');
 INSERT INTO Orders (orderID, userID, prodID, quantity, status, money_saved, order_datetime) VALUES (1421773477, 3, 2, 2, 'Pending', 0, '2019-10-31 13:00:00');
 INSERT INTO Orders (orderID, userID, prodID, quantity, status, money_saved, order_datetime) VALUES (1421773477, 3, 3, 3, 'Pending', 0, '2019-10-31 13:00:00');
